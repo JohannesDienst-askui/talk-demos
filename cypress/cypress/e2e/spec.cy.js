3b597087cd73f77f654ecb1c5f362cb0e8fd88f5
JohannesDienst-askui <johannes.dienst@askui.com>
2022-10-25 19:01:23 +0200

    More Cypress usage examples

--- cypress/cypress/e2e/spec.cy.js
+++ cypress/cypress/e2e/spec.cy.js
@@ -1,30 +1,24 @@
 describe('practice page spec', () => {
   it('types into textfield and shadow dom', () => {
 
-    cy.visit('../XpathPracticePage.html')
-    
+    cy.visit('https://selectorshub.com/xpath-practice-page/')
+
+    // XPath
+    cy.xpath('(//input[@name=\'company\'])[1]').type('mycompany')
+
+    // CSS
+    cy.get('#pass').type('mytotallySecretPa$$');
+
+    // Does not work because 'readonly' without focus
+    // cy.get('#userId').type('myUsername');
+
+    // Shadow DOM
+    cy.get('#userName').shadow().find('#kils').type('superuser')
+
+    // Click Buttons
+    cy.contains('Checkout here').click()
+
+    // Relational Selector
     cy.get('input').closest('#inp_val').type('Really close')
   })
 })
-
-
-// XPath
-// cy.xpath('(//input[@name=\'company\'])[1]').type('mycompany')
-
-// CSS
-// cy.get('#pass').type('mytotallySecretPa$$');
-
-// Does not work because 'readonly' without focus
-// cy.get('#userId').type('myUsername');
-
-// Shadow DOM
-// cy.get('#userName').shadow().find('#kils').type('superuser')
-
-// Click Buttons
-// cy.contains('Checkout here').click()
-
-// Relational Selector
-// cy.get('input').closest('#inp_val').type('Really close')
-
-
-// cy.visit('https://selectorshub.com/xpath-practice-page/')
