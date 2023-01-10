6b387e10a6bc2ea7af29ee8327cac0b40ee12f4b
JohannesDienst-askui <johannes.dienst@askui.com>
2023-01-10 12:08:08 +0100

    Final version of the android talk-demos for Testbusters Night

--- android/test/my-first-askui-test-suite.test.ts
+++ android/test/my-first-askui-test-suite.test.ts
@@ -1,11 +1,12 @@
+import { UiControlClient } from 'askui';
 import { aui, auiAndroid } from './helper/jest.setup';
 
 describe('jest with askui', () => {
 
-  it('annotate', async () => {
+  xit('annotate', async () => {
     await aui.annotateInteractively();
   });
-  
+
   xit ('automates Google Maps Canvas', async () => {
   
     // Automate search for Würzburg
@@ -46,17 +47,21 @@ describe('jest with askui', () => {
 
   xit('should fill up the textfields and push buttons', async () => {
 
-    await aui.click().button().withText('reset').exec();
+    await auiAndroid.click().button().withText('reset').exec();
+    await auiAndroid.waitFor(2000).exec();
     
     // click on the textfield and type characters
     // repeat this as many times as the textfields
-    await auiAndroid.click().text().withText('Enter your username').exec();
+    await auiAndroid.click().text().withTextRegex('nter[\\s]{0,1}your[\\s]{0,1}usernam').exec();
+    await auiAndroid.waitFor(1000).exec();
     await auiAndroid.type('askui').exec();
-    await auiAndroid.click().text().withText('Enter your email').exec();
+    await auiAndroid.click().text().withTextRegex('nter[\\s]{0,1}your[\\s]{0,1}emai').exec();
+    await auiAndroid.waitFor(1000).exec();
     await auiAndroid.type('askui@askui.com').exec();
     
     // Click and type the address
-    await auiAndroid.click().text().withText('Enter your address').exec();
+    await auiAndroid.click().text().withTextRegex('nter[\\s]{0,1}your[\\s]{0,1}addres').exec();
+    await auiAndroid.waitFor(1000).exec();
     await auiAndroid.type('Emmy-Noether-Straße 17').exec();
     
     // Pressing enter is the equivelant to pressing the return button on the on-screen-keyboard
@@ -64,7 +69,7 @@ describe('jest with askui', () => {
     await auiAndroid.pressAndroidKey('enter').exec();
     
     // Press the 'Submit' button
-    await auiAndroid.click().text().withText('Submit').exec();
+    await auiAndroid.click().button().withText('Submit').exec();
     
     // We will have a popup window that has two buttons. Press the 'Refuse' button
     await auiAndroid.click().text().withText('Refuse').exec();
@@ -77,13 +82,13 @@ describe('jest with askui', () => {
     await auiAndroid.click().text().withText('Windy').exec();
     
     // Fuzzy
-    await aui.click().button().withText('SubmIT').exec();
-    await aui.click().text().withText('Refuse').exec();
+    await auiAndroid.click().button().withText('Submit').exec();
+    await auiAndroid.click().text().withText('Refuse').exec();
     
-    await aui.moveMouseRelativelyTo(0, 100).text().withText("Windy").exec();
-    await aui.mouseToggleDown().exec();
-    await aui.moveMouseRelatively(-1500, 0).exec();
-    await aui.mouseToggleUp().exec();
+    await auiAndroid.moveMouseRelativelyTo(0, 100).text().withText("Windy").exec();
+    await auiAndroid.mouseToggleDown().exec();
+    await auiAndroid.moveMouseRelatively(-1500, 0).exec();
+    await auiAndroid.mouseToggleUp().exec();
   });
 
   xit('Login in Github with 2 FA and Lastpass App on Android device', async () => {
@@ -91,7 +96,7 @@ describe('jest with askui', () => {
     await aui.mouseLeftClick().exec();
     await aui.click().text().withText('Sign in').exec();
     await aui
-      .typeIn('askui-two-factor', { isSecret: true, secretMask: '**' })
+      .typeIn('<Github username>', { isSecret: true, secretMask: '**' })
       .textfield()
       .below()
       .text()
@@ -101,7 +106,7 @@ describe('jest with askui', () => {
       .pressKey('escape')
       .exec()
     await aui
-      .typeIn('S=uj<}aN?Yk&1', { isSecret: true, secretMask: '**' })
+      .typeIn('<Github password>', { isSecret: true, secretMask: '**' })
       .textfield()
       .below()
       .text()
@@ -135,7 +140,7 @@ describe('jest with askui', () => {
     const code = codeElements[0].text.replace(/\D/g, '');
     await aui
       .typeIn(code, { isSecret: true, secretMask: '**' })
-      .text().withText("XXXXXX")
+      .text().withText('XXXXXX')
       .above()
       .text()
       .withText('Verify')
@@ -156,4 +161,73 @@ async function waitUntil(askuiCommand: Promise<void>, maxTry = 5) {
     await aui.waitFor(2000).exec()
     await waitUntil(askuiCommand, maxTry - 1)
   }
+}
+
+async function execOnAndroidShell(keycode: number, uiControlClient: UiControlClient) {
+  await uiControlClient.execOnShell('input keyevent "' + keycode + '"').exec();
+}
+
+async function setText(text: String, uiControlClient: UiControlClient) {
+  for (var i = 0; i < text.length; i++) {
+      const elementCode = text.charCodeAt(i);
+      const elementChar = text.charAt(i);
+      console.log("setText()", elementCode);
+      console.log("setText()", elementChar);
+      if (elementCode >= 48 && elementCode <= 57){
+        /** 0~9 **/
+        await execOnAndroidShell(elementCode-41, uiControlClient);
+      }
+      else if (elementCode >= 65 && elementCode <= 90) {
+        /** A~Z **/
+        // TODO We have to press shift here somehow
+        await execOnAndroidShell(elementCode-36, uiControlClient);
+      }
+      else if (elementCode >= 97 && elementCode <= 122) {
+          /** a~z **/
+          await execOnAndroidShell(elementCode-68, uiControlClient);
+      }
+      else {
+        if (';' === elementChar) {
+          await execOnAndroidShell(74, uiControlClient);
+        }
+        if ('=' === elementChar) {
+          await execOnAndroidShell(70, uiControlClient);
+        }
+        if (',' === elementChar) {
+          await execOnAndroidShell(159, uiControlClient);
+        }
+        // '-'.code -> uiDevice.pressKeyCode(KEYCODE_MINUS)
+        // '.'.code -> uiDevice.pressKeyCode(KEYCODE_PERIOD)
+        // '/'.code -> uiDevice.pressKeyCode(KEYCODE_SLASH)
+        // '`'.code -> uiDevice.pressKeyCode(KEYCODE_GRAVE)
+        // '\''.code -> uiDevice.pressKeyCode(KEYCODE_APOSTROPHE)
+        // '['.code -> uiDevice.pressKeyCode(KEYCODE_LEFT_BRACKET)
+        // ']'.code -> uiDevice.pressKeyCode(KEYCODE_RIGHT_BRACKET)
+        // '\\'.code -> uiDevice.pressKeyCode(KEYCODE_BACKSLASH)
+        // ' '.code -> uiDevice.pressKeyCode(KEYCODE_SPACE)
+        if ('@' === elementChar) {
+          await execOnAndroidShell(77, uiControlClient);
+        }
+        // '#'.code -> uiDevice.pressKeyCode(KEYCODE_POUND)
+        // '*'.code -> uiDevice.pressKeyCode(KEYCODE_STAR)
+        // '('.code -> uiDevice.pressKeyCode(KEYCODE_NUMPAD_LEFT_PAREN)
+        // ')'.code -> uiDevice.pressKeyCode(KEYCODE_NUMPAD_RIGHT_PAREN)
+        // '+'.code -> uiDevice.pressKeyCode(KEYCODE_NUMPAD_ADD)
+        // '!'.code -> keyPressShiftedToEvents(uiDevice, KEYCODE_1)
+        // '$'.code -> keyPressShiftedToEvents(uiDevice, KEYCODE_4)
+        // '%'.code -> keyPressShiftedToEvents(uiDevice, KEYCODE_5)
+        // '^'.code -> keyPressShiftedToEvents(uiDevice, KEYCODE_6)
+        // '&'.code -> keyPressShiftedToEvents(uiDevice, KEYCODE_7)
+        // '"'.code -> keyPressShiftedToEvents(uiDevice, KEYCODE_APOSTROPHE)
+        // '{'.code -> keyPressShiftedToEvents(uiDevice, KEYCODE_LEFT_BRACKET)
+        // '}'.code -> keyPressShiftedToEvents(uiDevice, KEYCODE_RIGHT_BRACKET)
+        // ':'.code -> keyPressShiftedToEvents(uiDevice, KEYCODE_SEMICOLON)
+        // '|'.code -> keyPressShiftedToEvents(uiDevice, KEYCODE_BACKSLASH)
+        // '<'.code -> keyPressShiftedToEvents(uiDevice, KEYCODE_COMMA)
+        // '>'.code -> keyPressShiftedToEvents(uiDevice, KEYCODE_PERIOD)
+        // '?'.code -> keyPressShiftedToEvents(uiDevice, KEYCODE_SLASH)
+        // '~'.code -> keyPressShiftedToEvents(uiDevice, KEYCODE_GRAVE)
+        // '_'.code -> keyPressShiftedToEvents(uiDevice, KEYCODE_MINUS)
+      }
+    }
 }
